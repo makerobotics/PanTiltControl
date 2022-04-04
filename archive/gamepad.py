d78e5aacef0bb702d89fe812ecced0003369202b
makerobotics <yann.letzner@gmail.com>
2022-04-04 20:49:57 +0200

    Added key event listener (uses sudo)

--- archive/gamepad.py
+++ archive/gamepad.py
@@ -1,7 +1,15 @@
 from inputs import get_gamepad
+from inputs import devices
+from inputs import get_key
+
 while 1:
     try:
-        events = get_gamepad()
+        #events = devices.gamepads[0]._do_iter()
+        #if events is not None:            
+        #    for event in events:
+        #        print(event.code+": "+str(event.state))
+        #events = get_gamepad()
+        events = get_key()
         for event in events:
             #print(event.ev_type, event.code, event.state)
             #print("code : "+event.code) # axis
